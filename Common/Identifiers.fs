72163a622fbf9606896022394fab02adf3080bc8
Charles Joseph Christie II <charles.christie.ii@hey.com>
2026-01-12 23:18:21 -0500

    Change Identifiers from namespace to module and add new Identifier types

--- Common/Identifiers.fs
+++ Common/Identifiers.fs
@@ -1,4 +1,4 @@
-namespace ParagonRobotics.DiamondScout.Common.Identifiers
+module ParagonRobotics.DiamondScout.Common.Identifiers
 
 [<Struct>]
 type TeamId = TeamId of int
@@ -11,3 +11,18 @@ type EventId = EventId of int
 
 [<Struct>]
 type GameId = GameId of int
+
+[<Struct>]
+type PitResultId = PitResultId of int
+
+[<Struct>]
+type MatchResultId = MatchResultId of int
+
+[<Struct>]
+type InfractionId = InfractionId of int
+
+[<Struct>]
+type GamePieceId = GamePieceId of int
+
+[<Struct>]
+type MatchPhaseId = MatchPhaseId of int
