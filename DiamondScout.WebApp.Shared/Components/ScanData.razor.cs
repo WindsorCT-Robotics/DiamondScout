95632388636fad9ac5bf03e5c902dd6284d81c64
Elijah Jubrey <elijahjubrey@gmail.com>
2026-03-07 13:34:18 -0500

    Add stub data and QR scan functionality

--- DiamondScout.WebApp.Shared/Components/ScanData.razor.cs
+++ DiamondScout.WebApp.Shared/Components/ScanData.razor.cs
@@ -3,13 +3,22 @@ using ReactorBlazorQRCodeScanner;
 
 namespace DiamondScout.WebApp.Shared.Components;
 
-public partial class ScanData(Action<string> onScan) : ComponentBase
+public partial class ScanData : ComponentBase
 {
+    [Parameter]
+    public EventCallback<string> OnScan { get; set; }
+
     private QRCodeScannerJsInterop? _qrCodeScannerJsInterop;
-    
-    protected override async Task OnInitializedAsync()
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
     {
+        if (!firstRender)
+            return;
+
         _qrCodeScannerJsInterop = new QRCodeScannerJsInterop(Js);
-        await _qrCodeScannerJsInterop.Init(onScan);
+        await _qrCodeScannerJsInterop.Init(async scannedValue =>
+        {
+            await OnScan.InvokeAsync(scannedValue);
+        });
     }
 }
