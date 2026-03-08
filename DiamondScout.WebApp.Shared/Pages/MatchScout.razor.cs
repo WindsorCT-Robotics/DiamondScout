8c1e5b7ffabbf7a20ff9a6649c4ba5cd4e371075
Elijah Jubrey <elijahjubrey@gmail.com>
2026-03-08 13:21:28 -0700

    Add MatchScout to NavMenu
    Populate matchscout with auto fields

---
+++ DiamondScout.WebApp.Shared/Pages/MatchScout.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DiamondScout.WebApp.Shared.Pages;
+
+public partial class MatchScout : ComponentBase
+{
+    protected int? TeamNumber { get; set; }
+
+    protected string SelectedAlliance { get; set; } = "Red";
+    
+    protected string? StartingPosition { get; set; }
+
+    protected bool? PreloadedFuel { get; set; } = true;
+
+    protected IReadOnlyList<string> AllianceOptions { get; } =
+        new[] { "Red", "Blue" };
+    
+    protected IReadOnlyList<string> StartingOptions { get; } =
+        new[] { "Hub", "Bump", "Trench" };
+    
+    protected IReadOnlyList<string> PreloadFuelBool { get; } =
+        new[] { "Hub", "Bump", "Trench" };
+}
