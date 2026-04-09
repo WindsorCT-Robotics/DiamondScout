b98d95004733f19b0714e420b45c2808b0e33c46
Chaz Christie <Chaz.Christie@bio-techne.com>
2026-04-09 02:02:51 -0400

    Refactor FrcDistrictCode to remove unnecessary members and simplify structure

--- Common/FrcDistrict.fs
+++ Common/FrcDistrict.fs
@@ -4,15 +4,7 @@ open ParagonRobotics.DiamondScout.Common
 
 [<Struct>]
 type FrcDistrictCode =
-    private
     | FrcDistrictCode of string
-
-    member this.Value = let (FrcDistrictCode code) = this in code
-
-    static member Create(code: string) =
-        match code.Length with
-        | 3 -> FrcDistrictCode code
-        | _ -> invalidOp $"Invalid FRC district code: %s{code}"
 
 [<Struct>]
 type FrcDistrictName =
