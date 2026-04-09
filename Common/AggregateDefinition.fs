4ee559b880e9b1639d12d16501d162911d0575ba
Chaz Christie <Chaz.Christie@bio-techne.com>
2026-04-09 02:01:46 -0400

    Remove internal access modifiers from AggregateDefinition and its create function

--- Common/AggregateDefinition.fs
+++ Common/AggregateDefinition.fs
@@ -6,14 +6,13 @@ type Evolve<'state, 'event> = 'state -> 'event -> 'state
 type Decide<'state, 'command, 'event, 'error> = 'command -> 'state -> Validation<'event list, 'error>
 
 type AggregateDefinition<'state, 'command, 'event, 'error> =
-    internal
-        { Init: 'state
-          Evolve: Evolve<'state, 'event>
-          Decide: Decide<'state, 'command, 'event, 'error> }
+    { Init: 'state
+      Evolve: Evolve<'state, 'event>
+      Decide: Decide<'state, 'command, 'event, 'error> }
 
 [<RequireQualifiedAccess>]
 module AggregateDefinition =
-    let internal create initialState evolver decider =
+    let create initialState evolver decider =
         { Init = initialState
           Evolve = evolver
           Decide = decider }
